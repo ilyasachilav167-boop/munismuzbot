11cf44ef088f7b8915734aa4767a0fb7f445a0b8
ilyasachilav167-boop <ilyasachilav167@icloud.com>
2026-04-08 09:38:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import telebot
 import os
 
-# Telegram bot tokeningiz
-TOKEN = '7629555193:AAHkC869Wq_Vl867eS7t65F3_6eZ-wU80'
+# Render sozlamalaridan tokenni oladi
+TOKEN = os.getenv("BOT_TOKEN") 
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    user_name = message.from_user.first_name
     text = (
-        f"Assalomu alaykum, {message.from_user.first_name}!\n\n"
-        "🎶 Musiqa izlash botiga xush kelibsiz.\n\n"
-        "🌟 Hamkorimiz: @munisaparfumerie — Eng sifatli atirlar olami!\n"
-        "Musiqa topish uchun qo'shiq nomini yozing."
+        f"Assalomu alaykum, {user_name}!\n"
+        "🎶 Musiqa izlash botiga xush kelibsiz.\n"
+        "🌟 Hamkorimiz: @munisaparfumerie\n"
+        "Musiqa topish uchun qo‘shiq nomini yozing."
     )
     bot.reply_to(message, text)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, f"'{message.text}' bo'yicha musiqa qidirilmoqda...\n\nSifatli iforlar: @munisaparfumerie")
+    bot.reply_to(message, f"'{message.text}' bo‘yicha musiqa qidirilmoqda...")
 
 if __name__ == "__main__":
     bot.infinity_polling()
+
